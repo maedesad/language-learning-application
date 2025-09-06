e12823810c921a6134f21aac32d3c4a872f3881b
maedej2 <maedejalali2@gmail.com>
2025-09-06 14:44:35 +0430

    Adding a triangular arrow to the top of the pop-up rectangle.

--- frontend/lib/main_pages/practice/practice_entry_popup.dart
+++ frontend/lib/main_pages/practice/practice_entry_popup.dart
@@ -8,13 +8,12 @@ class PracticePopup {
     required BuildContext context,
     required GlobalKey buttonKey,
     required String title,
-    required Widget practiceEntryPage, // صفحه مقصد
+    required Widget practiceEntryPage,
     bool isTwoButtonMode = true,
     PopupArrowPosition arrowPosition = PopupArrowPosition.center,
     PopupDirection direction = PopupDirection.down,
     VoidCallback? onHide,
   }) {
-    // بستن پاپ‌آپ قبلی اگه وجود داشته باشه
     hidePopup();
 
     final RenderBox buttonBox =
@@ -22,30 +21,31 @@ class PracticePopup {
     final Offset buttonPosition = buttonBox.localToGlobal(Offset.zero);
     final Size buttonSize = buttonBox.size;
 
+    const double popupWidth = 200;
+    const double popupHeight = 140;
+    const double arrowSize = 12;
+
+    double left;
+    if (arrowPosition == PopupArrowPosition.left) {
+      left = buttonPosition.dx;
+    } else if (arrowPosition == PopupArrowPosition.right) {
+      left = buttonPosition.dx + buttonSize.width - popupWidth;
+    } else {
+      left = buttonPosition.dx + buttonSize.width / 2 - popupWidth / 2;
+    }
+
+    double top = direction == PopupDirection.down
+        ? buttonPosition.dy + buttonSize.height + arrowSize
+        : buttonPosition.dy - popupHeight - arrowSize;
+
     _currentPopup = OverlayEntry(
       builder: (context) {
-        // تعیین موقعیت پاپ‌آپ
-        double top = direction == PopupDirection.down
-            ? buttonPosition.dy + buttonSize.height + 8
-            : buttonPosition.dy - 140; // ارتفاع تقریبی پاپ‌آپ
-
-        double left;
-        if (arrowPosition == PopupArrowPosition.left) {
-          left = buttonPosition.dx;
-        } else if (arrowPosition == PopupArrowPosition.right) {
-          left = buttonPosition.dx + buttonSize.width - 200;
-        } else {
-          left = buttonPosition.dx + buttonSize.width / 2 - 100;
-        }
-
         return Stack(
           children: [
-            
-            // خود پاپ‌آپ
             Positioned(
               top: top,
               left: left,
-              width: 200,
+              width: popupWidth,
               child: Material(
                 color: Colors.transparent,
                 child: _PopupContent(
@@ -53,6 +53,7 @@ class PracticePopup {
                   arrowPosition: arrowPosition,
                   direction: direction,
                   isTwoButtonMode: isTwoButtonMode,
+                  arrowSize: arrowSize,
                   onPurpleButtonTap: () {
                     hidePopup();
                     Navigator.push(
@@ -86,6 +87,7 @@ class _PopupContent extends StatelessWidget {
   final bool isTwoButtonMode;
   final PopupArrowPosition arrowPosition;
   final PopupDirection direction;
+  final double arrowSize;
 
   const _PopupContent({
     required this.title,
@@ -93,13 +95,15 @@ class _PopupContent extends StatelessWidget {
     required this.isTwoButtonMode,
     required this.arrowPosition,
     required this.direction,
+    required this.arrowSize,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
+      clipBehavior: Clip.none,
       children: [
-        if (direction == PopupDirection.up) _buildArrow(),
+        // مستطیل اصلی پاپ‌آپ
         Container(
           decoration: BoxDecoration(
             color: Colors.grey[800],
@@ -108,13 +112,14 @@ class _PopupContent extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
               Text(title,
                   style: const TextStyle(color: Colors.white, fontSize: 16)),
               const SizedBox(height: 12),
               if (isTwoButtonMode) ...[
                 ElevatedButton(
-                  onPressed: null, // غیرفعال
+                  onPressed: null,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue[200],
                   ),
@@ -132,30 +137,59 @@ class _PopupContent extends StatelessWidget {
             ],
           ),
         ),
-        if (direction == PopupDirection.down) _buildArrow(),
+
+        // فلش
+        Positioned(
+          top: direction == PopupDirection.down ? -arrowSize : null,
+          bottom: direction == PopupDirection.up ? -arrowSize : null,
+          left: _calculateArrowLeft(),
+          child: CustomPaint(
+            size: Size(arrowSize, arrowSize),
+            painter: _ArrowPainter(direction, Colors.grey[800]!),
+          ),
+        ),
       ],
     );
   }
 
-  Widget _buildArrow() {
-    Alignment alignment;
-    if (arrowPosition == PopupArrowPosition.left) {
-      alignment = Alignment.centerLeft;
-    } else if (arrowPosition == PopupArrowPosition.right) {
-      alignment = Alignment.centerRight;
-    } else {
-      alignment = Alignment.center;
+  double _calculateArrowLeft() {
+    switch (arrowPosition) {
+      case PopupArrowPosition.left:
+        return 16;
+      case PopupArrowPosition.right:
+        return 200 - arrowSize - 16;
+      case PopupArrowPosition.center:
+      default:
+        return 200 / 2 - arrowSize / 2;
     }
-
-    return Align(
-      alignment: alignment,
-      child: Icon(
-        direction == PopupDirection.down
-            ? Icons.arrow_drop_up
-            : Icons.arrow_drop_down,
-        color: Colors.grey[800],
-        size: 32,
-      ),
-    );
   }
+}
+
+class _ArrowPainter extends CustomPainter {
+  final PopupDirection direction;
+  final Color color;
+
+  _ArrowPainter(this.direction, this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = color;
+    final path = Path();
+    if (direction == PopupDirection.down) {
+      // مثلث به سمت بالا
+      path.moveTo(0, size.height);
+      path.lineTo(size.width / 2, 0);
+      path.lineTo(size.width, size.height);
+    } else {
+      // مثلث به سمت پایین
+      path.moveTo(0, 0);
+      path.lineTo(size.width / 2, size.height);
+      path.lineTo(size.width, 0);
+    }
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
